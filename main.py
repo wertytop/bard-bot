664c5626ba35b5375559435b5955442fb2c8b36a
wertytop <wertytop08@gmail.com>
2023-07-01 12:33:52 -0700

    Added a few util things, and fun commands

--- main.py
+++ main.py
@@ -17,29 +17,52 @@ async def bard(self, prompt: str):
     await self.response.defer()
     load_dotenv()
     token = os.getenv("BARD_TOKEN")
-    bard = Bard(token=token)
-    print(prompt)
-    answer = bard.get_answer(prompt+", you must keep this under 1000 characters")['content']
-    print(answer, prompt)
+    bardbot = Bard(token=token)
+    answer = bardbot.get_answer(prompt + ", you must keep this under 1000 characters")['content']
+    print(f'{self.user} used the bard command with the parameter \'{prompt}\'. | ' + str(datetime.datetime.now()))
     await self.followup.send(answer)
 
 
 @tree.command(name="roll", description="rolls a d6")
 async def roll(self):
     await self.response.send_message(str(random.randint(1, 6)) + " :game_die:")
+    print(f'{self.user} used the roll command | ' + str(datetime.datetime.now()))
 
 
 @tree.command(name="date", description="gives you the current date")
 async def date(self):
-    date = datetime.datetime.now()
-    await self.response.send_message(date.strftime("%A") + " " + date.strftime("%B") + " " + date.strftime("%d"))
+    dates = datetime.datetime.now()
+    await self.response.send_message(dates.strftime("%A") + " " + dates.strftime("%B") + " " + dates.strftime("%d"))
+    print(f'{self.user} used the date command | ' + str(datetime.datetime.now()))
+
+
+@tree.command(name="fight", description="fights annother user")
+async def fight(self, user: discord.Member):
+    await self.response.defer()
+    coinflips = [
+        "**{winner}** was eaten on a plate **{loser}** ",
+        "**{winner}** was bed bombed by **{loser}** ",
+        "**{winner}** was pushed into the void **{loser}** ",
+        "**{winner}** was dropkicked by **{loser}** ",
+        "**{winner}** was DDoSed by **{loser}** (ethan refrence)",
+        "**{winner}** was demolished by **{loser}** ",
+        "**{winner}** was critted out with a diamond axe by **{loser}** ",
+    ]
+    rand = random.randint(1, 2)
+    attacker = self.user
+    if rand == 1:
+        await self.followup.send(random.choice(coinflips).format(winner=user, loser=attacker))
+    else:
+        await self.followup.send(random.choice(coinflips).format(loser=user, winner=attacker))
 
 
 @client.event
 async def on_ready():
     print("Bot has booted!")
+    print({str(client.guilds)})
     await tree.sync()
-    await client.change_presence(activity=discord.Game(name='Listening for /bard'))
+    await client.change_presence(
+        activity=discord.Game(name='Listening for /bard | ' + str(len(client.guilds)) + " servers"))
 
 
 load_dotenv()
